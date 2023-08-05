7f9a25cf0f7596bb6661850734e6889237e15c72
Keshav Agarwal <keshav.agarwal422@gmail.com>
2023-08-06 00:31:35 +0530

    Update Introduction.js

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Introduction = () => {
     return (
-        <div className="w-full h-full flex flex-col mt-[5vh] items-center justify-start">
-            <div className="h-[5vh] w-[20vw] rounded-3xl bg-[#717171] flex justify-center items-center text-[4vw]">Walking</div>
-            <div className='mt-[25vh] text-center'>
+        <div className="w-full h-full flex flex-col mt-[10vw] items-center justify-start">
+            <div className="h-[10vw] w-[20vw] rounded-3xl bg-[#717171] flex justify-center items-center text-[4vw]">Walking</div>
+            <div className='mt-[45vw] text-center'>
                 <h1 className='text-[10vw]'>CarbonCrusher</h1>
                 <span className='text-[3.5vw]'>Reshaping the Future, Sustaining the Earth</span>
             </div>
